c2a341db3e1c9164c3fd79565bfc2aad8385303b
jchang2415 <142466647+jchang2415@users.noreply.github.com>
2025-11-01 15:09:31 -0400

    Add files via upload
    Empty placeholder file for organization purposes.
    
    Will define function that generates output file containing the solution.

---
+++ output.py
@@ -0,0 +1,11 @@
+# Solution Output Generation #
+
+# File defining function for writing the found solution out to an output text file
+
+# Empty placeholder file for organization
+
+def write_solution(solution, filename = "solution.txt"):
+
+    with open(filename, "w") as outfile:
+
+        pass
