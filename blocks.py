0abb1d53410bb40f12e080a2199c7fba5197c6ea
Ccash6-maker <Ccash6@jh.edu>
2025-11-03 15:36:16 -0500

    Add type hints to Block class methods
    Updated Block classes to use type hints for parameters and return types.

--- blocks.py
+++ blocks.py
@@ -1,6 +1,9 @@
+#add import dict, tuple, list
+from typing import Dict, Tuple, List
+
 class Block:
     kind: str
-    def interact(self, pos, dir, vert_edge: bool):
+    def interact(self, pos: Tuple[int, int], dir: Tuple[int, int], vert_edge: bool) -> List[Tuple[Tuple[int, int], Tuple[int, int]]]:
         raise NotImplementedError
 
 class ReflectBlock(Block):
@@ -10,8 +13,8 @@ class ReflectBlock(Block):
         - horizontal block face  (odd x, even y): flip vy -> ( vx, −vy)
     """
     kind = 'A'
-    def interact(self, pos, dir, vert_edge: bool):
-        vx,vy = dir.x, dir.y
+    def interact(self, pos: Tuple[int, int], dir: Tuple[int, int], vert_edge: bool):
+        vx, vy = dir
         if vert_edge:
             return [(pos, (-vx, vy))]
         else:
@@ -23,19 +26,20 @@ class OpaqueBlock(Block):
     """
     kind = 'B'
 
-    def interact(self, pos, dir, vert_edge: bool):
-        return []
+    def interact(self, pos: Tuple[int, int], dir: Tuple[int, int], vert_edge: bool):
+        return [] #returns empty list(the laser is absorbed)
 
 class RefractBlock(Block):
     """
         C: refracts lazor, allows lazor to pass through and reflects at 90° like ReflectBlock class
     """
     kind = 'C'
-    def interact(self, pos, dir, vert_edge: bool):
-        vx, vy = dir.x, dir.y
+    def interact(self, pos: Tuple[int, int], dir: Tuple[int, int], vert_edge: bool):
+        vx, vy = dir
         if vert_edge:
-            return [(pos,dir),(pos,(-vx, vy))]
+            return [(pos, dir), (pos, (-vx, vy))]
         else:
-            return [(pos,dir),(pos,(vx, -vy))]
+            return [(pos, dir), (pos, (vx, -vy))]
 
 BLOCKS: Dict[str, Block] = { 'A': ReflectBlock(), 'B': OpaqueBlock(), 'C': RefractBlock(),}
+
